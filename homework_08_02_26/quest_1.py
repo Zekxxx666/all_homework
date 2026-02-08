33770ce64f661c0015eee516974b18363f66ed15
Zekxxx666 <Zyekov35@gmail.com>
2026-02-08 16:25:18 +0300

    Update and fix quest_1

--- homework_08_02_26/quest_1.py
+++ homework_08_02_26/quest_1.py
@@ -6,15 +6,33 @@ ID = 4
 class GamePC:
     id: int
     fabricator: str
-    CPU: str
-    GPU: str
-    RAM: int
-    SSD: int
+    cpu: str
+    gpu: str
+    ram: int
+    ssd: int
     weight: int
     price: int
     count: int
 
 PCs = []
+
+def find_ind_PC_by_id(PCs, id):
+    for i in range(len(PCs)):
+        if PCs[i].id == id:
+            return i
+    else:
+        return -1
+    
+def input_correct(text):
+    is_correct_input = False
+    while is_correct_input == False:
+        try:
+            result = int(input(text))
+            is_correct_input = True
+        except:
+            print("Ошибка ввода, попробуй ещё раз")
+            is_correct_input = False
+    return result
 
 def add_PC(PCs, PC):
     global ID
@@ -27,36 +45,26 @@ def add_PC(PCs, PC):
     os.system("cls")
 
 def parameter_input():
+    fabricator = input("введите производителя:")
+    cpu = input("введите название процессора:")
+    gpu = input_correct("введите количество видеопамяти графического процессора:")
+    ram = input_correct("введите количество оперативной памяти:")
+    ssd = input_correct("введите количество внутренней памяти:")
+    weight = input_correct("введите вес компьютера:")
+    price = input_correct("введите стоимость компьютера:")
+    count = input_correct("введите количество товара на складе:")
 
-    is_correct_input = False
-
-    while is_correct_input == False:
-        try:
-            fabricator = input("введите производителя:")
-            CPU = input("введите название процессора:")
-            GPU = int(input("введите количество видеопамяти графического процессора:"))
-            RAM = int(input("введите количество оперативной памяти:"))
-            SSD = int(input("введите количество внутренней памяти:"))
-            weight = int(input("введите вес компьютера:"))
-            price = int(input("введите стоимость компьютера:"))
-            count = int(input("введите количество товара на складе:"))
-
-            return GamePC(0, fabricator, CPU, GPU, RAM, SSD, weight, price, count)
-            
-        except:
-            os.system("cls")
-            print(f"ошибка ввода. Попробуйте ввести ещё раз")
-            is_correct_input = False
+    return GamePC(0, fabricator, cpu, gpu, ram, ssd, weight, price, count)
             
 def print_PC(PCs):
     os.system("cls")
     print(f"{'ID':<8}{'Изготовитель':<15}{'CPU':<15}{'GPU':<15}{'RAM':<15}{'SSD':<15}{'вес':<7}{'цена':<10}{'количество товаров':<15}")
 
     for i in range(len(PCs)):
-        print(f"{PCs[i].id:<8}{PCs[i].fabricator:<15}{PCs[i].CPU:<15}{PCs[i].GPU:<15}{PCs[i].RAM:<15}{PCs[i].SSD:<15}{PCs[i].weight:<7}{PCs[i].price:<10}{PCs[i].count:<15}")
+        print(f"{PCs[i].id:<8}{PCs[i].fabricator:<15}{PCs[i].cpu:<15}{PCs[i].gpu:<15}{PCs[i].ram:<15}{PCs[i].ssd:<15}{PCs[i].weight:<7}{PCs[i].price:<10}{PCs[i].count:<15}")
 
 def print_best_bat(PCs):
-    best = 0
+    best = PCs[0].price
     bad = PCs[0].price
     ind_best = 0
     ind_bad = 0
@@ -73,37 +81,34 @@ def print_best_bat(PCs):
 
     print(f"{'ID':<8}{'Изготовитель':<15}{'CPU':<15}{'GPU':<15}{'RAM':<15}{'SSD':<15}{'вес':<7}{'цена':<10}{'количество товаров':<15}")
     print("Самый дорогой:")
-    print(f"{PCs[ind_best].id:<8}{PCs[ind_best].fabricator:<15}{PCs[ind_best].CPU:<15}{PCs[ind_best].GPU:<15}{PCs[ind_best].RAM:<15}{PCs[ind_best].SSD:<15}{PCs[ind_best].weight:<7}{PCs[ind_best].price:<10}{PCs[ind_best].count:<15}")
+    print(f"{PCs[ind_best].id:<8}{PCs[ind_best].fabricator:<15}{PCs[ind_best].cpu:<15}{PCs[ind_best].gpu:<15}{PCs[ind_best].ram:<15}{PCs[ind_best].ssd:<15}{PCs[ind_best].weight:<7}{PCs[ind_best].price:<10}{PCs[ind_best].count:<15}")
     print("Самый дешёвый:")
-    print(f"{PCs[ind_bad].id:<8}{PCs[ind_bad].fabricator:<15}{PCs[ind_bad].CPU:<15}{PCs[ind_bad].GPU:<15}{PCs[ind_bad].RAM:<15}{PCs[ind_bad].SSD:<15}{PCs[ind_bad].weight:<7}{PCs[ind_bad].price:<10}{PCs[ind_bad].count:<15}")        
+    print(f"{PCs[ind_bad].id:<8}{PCs[ind_bad].fabricator:<15}{PCs[ind_bad].cpu:<15}{PCs[ind_bad].gpu:<15}{PCs[ind_bad].ram:<15}{PCs[ind_bad].ssd:<15}{PCs[ind_bad].weight:<7}{PCs[ind_bad].price:<10}{PCs[ind_bad].count:<15}")        
 
 def up_RAM(PCs, id, up):
     os.system("cls")
-    for i in range(len(PCs)):
-        if PCs[i].id == id:
-            PCs[i].RAM += up
+    ind = find_ind_PC_by_id(PCs, id)
+    if ind != -1:
+        PCs[ind].ram += up
         print(f"Изменение памяти компьютера с id {id} успешно завершено")
-        break
     else:
         print(f"компьютера с id {id} не существует")
 
 def sale(PCs, id):
     os.system("cls")
-    for ind in range(len(PCs)):
-        if PCs[ind].id == id:
-            PCs[ind].price -= PCs[ind].price * 0.1
-            print(f"Компьютер с id {id} выстовлен на распрадажу")
-            break
+    ind = find_ind_PC_by_id(PCs, id)
+    if ind != -1:
+        PCs[ind].price -= PCs[ind].price * 0.1
+        print(f"Компьютер с id {id} выстовлен на распрадажу")
     else:
         print(f"компьютера с id {id} не существует")
 
 def del_PC_id(PCs, id):
     os.system("cls")
-    for ind in range(len(PCs)):
-        if PCs[ind].id == id:
-            PCs.pop(ind)
-            print(f"Компьютер с id {id} успешно удален")
-            break
+    ind = find_ind_PC_by_id(PCs, id)
+    if ind != -1:
+        PCs.pop(ind)
+        print(f"Компьютер с id {id} успешно удален")
     else:
         print(f"компьютера с id {id} не существует")
 
@@ -118,35 +123,33 @@ def print_ind_PC(PCs, ind):
     os.system("cls")
     try:
         print(f"{'ID':<8}{'Изготовитель':<15}{'CPU':<15}{'GPU':<15}{'RAM':<15}{'SSD':<15}{'вес':<7}{'цена':<10}{'количество товаров':<15}")
-        print(f"{PCs[ind].id:<8}{PCs[ind].fabricator:<15}{PCs[ind].CPU:<15}{PCs[ind].GPU:<15}{PCs[ind].RAM:<15}{PCs[ind].SSD:<15}{PCs[ind].weight:<7}{PCs[ind].price:<10}{PCs[ind].count:<15}")
+        print(f"{PCs[ind].id:<8}{PCs[ind].fabricator:<15}{PCs[ind].cpu:<15}{PCs[ind].gpu:<15}{PCs[ind].ram:<15}{PCs[ind].ssd:<15}{PCs[ind].weight:<7}{PCs[ind].price:<10}{PCs[ind].count:<15}")
     except:
         print(f"Компьютера с индексом {ind} не существует")
 
 def compare_GPU(PCs, min_har):
     print_only_heading()
     for ind in range(len(PCs)):
-        if PCs[ind].GPU > min_har:
+        if PCs[ind].gpu > min_har:
             print_ind_PC(PCs, ind)
 
 def print_only_heading():
     print(f"{'ID':<8}{'Изготовитель':<15}{'CPU':<15}{'GPU':<15}{'RAM':<15}{'SSD':<15}{'вес':<7}{'цена':<10}{'количество товаров':<15}")
 
 def print_1_pc(PC):
-    print(f"{PC.id:<8}{PC.fabricator:<15}{PC.CPU:<15}{PC.GPU:<15}{PC.RAM:<15}{PC.SSD:<15}{PC.weight:<7}{PC.price:<10}{PC.count:<15}")
+    print(f"{PC.id:<8}{PC.fabricator:<15}{PC.cpu:<15}{PC.gpu:<15}{PC.ram:<15}{PC.ssd:<15}{PC.weight:<7}{PC.price:<10}{PC.count:<15}")
 
 def sorted_PC_price(PCs):
-    j = 1
+    sort = True
     mid_glass = 0
-    while True:
-        if j == 0:
-            break
-        j = 0
+    while sort:
+        sort = False
         for i in range(len(PCs) - 1):
             if PCs[i].price < PCs[i + 1].price:
                 mid_glass = PCs[i + 1]
                 PCs[i + 1] = PCs[i]
                 PCs[i] = mid_glass
-                j += 1
+                sort = True
     print_PC(PCs)
 
 def searc_PC(PCs, min_ram, max_price):
@@ -155,15 +158,15 @@ def searc_PC(PCs, min_ram, max_price):
     
     results = []
     for pc in PCs:
-        if pc.RAM >= min_ram and pc.price <= max_price:
+        if pc.ram >= min_ram and pc.price <= max_price:
             results.append(pc)
     
     if results:
         print(f"\nНайдено {len(results)} компьютеров с ОЗУ ≥ {min_ram}ГБ и ценой ≤ {max_price}:")
         print_PC(results)
     else:
-        print(f"\nНет компьютеров с ОЗУ ≥ {min_ram}ГБ и ценой ≤ {max_price}")
-            
+        print(f"\nНет компьютеров с ОЗУ ≥ {min_ram}ГБ и ценой ≤ {max_price}")         
+
 def menu():
     global GAME_RUN
     print("======= МЕНЮ =======")
@@ -188,16 +191,8 @@ def menu():
         print_PC(PCs)
     elif result == 3:
         os.system("cls")
-        is_correct_input = False
-        while is_correct_input == False:
-            try:
-                min_ram = int(input("введите минимальное количество оперативной памяти в пк (ГБ):"))
-                max_price = int(input("введите максимальную стоимость компьютера:")) 
-                is_correct_input = True
-            except:
-                is_correct_input = False
-                print("Ошибка ввода попробуйте ещё раз")
-
+        min_ram = input_correct("введите минимальное количество оперативной памяти в пк (ГБ):")
+        max_price = input_correct("введите максимальную стоимость компьютера:")
         searc_PC(PCs, min_ram, max_price)
     elif result == 4:
         os.system("cls")
@@ -207,75 +202,28 @@ def menu():
         print_best_bat(PCs)
     elif result == 6:
         os.system("cls")
-        is_correct_input = False
-        while is_correct_input == False:
-            os.system("cls")
-            try:
-                id = int(input("введите ID ПК:"))
-                up_ram = int(input("введите количество оперативной памяти которую вы хотите добавить:"))
-                is_correct_input = True
-            except:
-                is_correct_input = False
-                print("ошибка ввода попробуйте ещё раз")
+        id = input_correct("введите ID ПК:")
+        up_ram = input_correct("введите количество оперативной памяти которую вы хотите добавить:")
         up_RAM(PCs, id, up_ram)
     elif result == 7:
         os.system("cls")
-        is_correct_input = False
-        while is_correct_input == False:
-            try:
-                result_7 = int(input("как вы хотите удалить пк (1 - по ID, 2 - по индексу)"))
-                is_correct_input = True
-            except:
-                is_correct_input = False
-                os.system("cls")
-                print("Ошибка ввода попробуйте ещё раз")
+        result_7 = input_correct("как вы хотите удалить пк (1 - по ID, 2 - по индексу):")
         if result_7 == 1:
             id_PC = 0
             os.system("cls")
-            is_correct_input = False
-            while is_correct_input == False:
-                try:
-                    id_PC = int(input("введите ID ПК:"))
-                    is_correct_input = True
-                except:
-                    os.system("cls")
-                    is_correct_input = False
-                    print("Ошибка ввода попробуйте ещё раз")
+            id_PC = input_correct("введите ID ПК:")
             del_PC_id(PCs, id_PC)
         else:
             os.system("cls")
-            is_correct_input = False
-            while is_correct_input == False:
-                try:
-                    ind_PC = int(input("введите INDEX ПК:"))
-                    is_correct_input = True
-                except:
-                    os.system("cls")
-                    is_correct_input = False
-                    print("Ошибка ввода попробуйте ещё раз")
+            ind_PC = input_correct("введите INDEX ПК:")
             del_PC_ind(PCs, ind_PC)
     elif result == 8:
         os.system("cls")
-        is_correct_input = False
-        while is_correct_input == False:
-            try:
-                sale_id = int(input("введте ID ПК которому вы хотите сделать скидку:"))
-                is_correct_input = True
-            except:
-                os.system("cls")
-                is_correct_input = False
-                print("Ошибка ввода попробуйте ещё раз")
+        sale_id = input_correct("введте ID ПК которому вы хотите сделать скидку:")
         sale(PCs, sale_id)
     elif result == 9:
         os.system("cls")
-        is_correct_input = False
-        while is_correct_input == False:
-            try:
-                har = int(input("введите минимальную характеристику видеокарте (ГБ видеопамяти):"))
-                is_correct_input = True
-            except:
-                is_correct_input = False
-                print("Ошибка ввода попробуйте ещё раз")
+        har = input_correct("введите минимальную характеристику видеокарте (ГБ видеопамяти):")
         compare_GPU(PCs, har)
     else:
         os.system("cls")
